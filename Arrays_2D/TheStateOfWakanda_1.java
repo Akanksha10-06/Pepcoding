e42553fe15b40b4c10d7c5c50da63ef3f3b858ff
Akanksha10-06 <95533415+Akanksha10-06@users.noreply.github.com>
2021-12-04 21:44:27 +0530

    Update TheStateOfWakanda_1.java

--- Arrays_2D/TheStateOfWakanda_1.java
+++ Arrays_2D/TheStateOfWakanda_1.java
@@ -2,39 +2,29 @@ package Arrays_2D;
 import java.util.Scanner;
 public class TheStateOfWakanda_1 {
     public static void main(String[] args) {
-        int[][] a=array_input();
-        int n=a.length;
-        int m=a[0].length;
-
+        Scanner s=new Scanner(System.in);
+        System.out.println("Number of rows: ");
+        int n=s.nextInt();
+        System.out.println("Number of columns: ");
+        int m=s.nextInt();
+        int[][] a=new int[n][m];
         for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                a[i][j]=s.nextInt();
+            }
+        }
+
+        for(int i=0; i<m; i++){
             if(i%2==0){
-                for(int j=0; j<m; j++){
-                    System.out.println(a[i][j]);
+                for(int j=0; j<n; j++){
+                    System.out.println(a[j][i]);
                 }
             }else{
-                for(int j=m-1; j>=0; j--){
-                    System.out.println(a[i][j]);
+                for(int j=n-1; j>=0; j--){
+                    System.out.println(a[j][i]);
                 }
             }
         }
-        
-    }
-
-    public static int[][] array_input(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter number of rows: ");
-        int n=s.nextInt();
-        System.out.println("Enter number of cols: ");
-        int m=s.nextInt();
-        int[][] arr=new int[n][m];
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                arr[i][j]=s.nextInt();
-            }
-        }
-
-        return arr;
     }
 
 }
