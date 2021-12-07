7d29e0cf4c2af8e333e14826e8f897b6e124a4d9
Akanksha Mishra <95533415+Akanksha10-06@users.noreply.github.com>
2021-12-07 20:42:49 +0530

    pattern 1

--- PATTERNS/Pattern1.java
+++ PATTERNS/Pattern1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Pattern1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the number of rows: ");
+        System.out.println("Enter the number of rows:- ");
         int n=s.nextInt();
 
         for(int i=1; i<=n; i++){
