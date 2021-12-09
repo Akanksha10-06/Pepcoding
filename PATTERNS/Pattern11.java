2bc4bb29edd87daa9df277cbc0bd45a772907769
Akanksha Mishra <95533415+Akanksha10-06@users.noreply.github.com>
2021-12-09 20:47:41 +0530

    Pattern11

---
+++ PATTERNS/Pattern11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Pattern11 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = s.nextInt();
+        int out=1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(out + " ");
+                out++;
+            }
+            System.out.println();
+        }
+    }
+}
